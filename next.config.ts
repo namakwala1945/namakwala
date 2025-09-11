b0f1d0c41efa877568e8d6e696642ad26321e554
nitink-dev <61575697+nitink-dev@users.noreply.github.com>
2025-09-11 14:58:38 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 const { i18n } = require('./next-i18next.config');
 
 module.exports = {
-  // output: "export", // 👈 required for static hosting
   images: {
-    unoptimized: true, // 👈 needed because next/image needs optimization server
+    unoptimized: true,
     domains: ["images.unsplash.com", "img.youtube.com"],
     remotePatterns: [
       { protocol: "https", hostname: "picsum.photos" },
@@ -14,5 +13,8 @@ module.exports = {
   i18n: {
     locales: ["en", "fr", "de", "ar", "ur", "af"],
     defaultLocale: "en",
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
