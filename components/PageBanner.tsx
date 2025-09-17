12adab63c57d143b1bc99eafd4e68fb017273acc
namakwala1945 <santosh.lohia@gmail.com>
2025-09-17 10:49:36 +0530

    banner issue resolve

--- components/PageBanner.tsx
+++ components/PageBanner.tsx
@@ -13,7 +13,6 @@ export default function PageBanner({ title, image, category }: PageBannerProps) 
   return (
     <div className="relative w-full h-64 md:h-80 bg-gray-100 flex items-center justify-center overflow-hidden shadow-md">
       {/* Background Image */}
-      <div className="relative w-full h-[100dvh]">
         <Image
           src={image}
           alt={title}
@@ -25,9 +24,6 @@ export default function PageBanner({ title, image, category }: PageBannerProps) 
           blurDataURL="/assets/placeholder.png"
           className="object-cover object-top"
         />
-      </div>
-
-
       {/* Overlay Content */}
       <div className="relative w-11/12 md:w-4/5 mx-auto z-10 text-center text-white animate-fade-in">
         <h2 className="text-2xl md:text-4xl font-bold playfair">{title}</h2>
