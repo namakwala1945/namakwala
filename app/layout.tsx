fb29d6af706958f5c47fa6a791ef5aa100a21e98
namakwala1945 <santosh.lohia@gmail.com>
2025-09-18 13:37:15 +0530

    Revert "salt-animation change add simple image"
    This reverts commit d5ca1bbc5154837e06ec81dcc8c9f68db40f0f0c.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,23 @@
+import { Playfair_Display, Poppins } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
 import { Providers } from "./providers";
 import ContentProtector from "./ContentProtector"; 
+
+// Load Playfair Display
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap", // prevents render-blocking
+});
+
+// Load Poppins
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  display: "swap",
+});
 
 export const metadata = {
   title: {
@@ -34,13 +49,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           name="viewport"
           content="width=device-width, initial-scale=1, viewport-fit=cover"
         />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Playfair+Display:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        />
-
       </head>
-      <body className="font-sans">
+      <body className={`${poppins.className} ${playfair.className}`}>
         {/* <ContentProtector /> */}
         <Header />
         <Providers>{children}</Providers>
