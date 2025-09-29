3455df66e74a93d7eb033e0f17968ae67553e5ac
namakwala1945 <santosh.lohia@gmail.com>
2025-09-29 10:55:38 +0530

    add layout title and description for google test speed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
 import CustomCursor from "../components/Cursor"; // ✅ Import cursor
+
+export const metadata: Metadata = {
+  title: "Best Salt's Exporter in India | Best Mineral's Exporter in India | NAMAKWALA", // ✅ Default title
+  description: "NAMAKWALA – The Best Salt's Exporter in India & Best Mineral's Exporter in India, delivering pure, high-quality salts & minerals worldwide with trust.",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
