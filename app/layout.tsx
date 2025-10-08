2454306654234eaae5406fa82559446b8aea3d0f
namakwala1945 <santosh.lohia@gmail.com>
2025-10-08 18:19:03 +0530

    add google tagmanager

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { Providers } from "./providers";
 import ClientComponents from "./ClientComponents"; // client-only wrapper
 import { Playfair_Display, Poppins } from "next/font/google";
+import Script from "next/script";
 
 // Fonts
 const playfair = Playfair_Display({
@@ -36,10 +37,33 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           name="viewport"
           content="width=device-width, initial-scale=1, viewport-fit=cover"
         />
+       {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W28M4F5C');
+          `}
+        </Script>
       </head>
       <body
         className={`${poppins.variable} ${playfair.variable} font-poppins font-playfair cursor-none`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W28M4F5C"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <ClientComponents /> {/* client-only features */}
         <Header />
         <Providers>{children}</Providers>
